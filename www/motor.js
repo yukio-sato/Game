d591cccc5aca0ae77473ecbbed14870e5b3b4b3e
yukio-sato <yukioutiyamasato@gmail.com>
2023-04-14 03:50:30 +0900

    \[13/04/2023\] Utilizando CSS

--- www/motor.js
+++ www/motor.js
@@ -1,23 +1,6 @@
+var atas = 0;
 function teste()
 {
-    document.getElementById("tecla").textContent =  document.getElementById("input").value+" Pressionado";
-    var tecla = document.getElementById("input").value;
-    document.getElementById("input").value = "";
-    if (tecla == "w")
-    {
-        document.getElementById("comando").textContent =  "ˆ";
-        document.getElementById("block").style.left = document.getElementById("block").style.left.valueOf.toString()+"10vw";
-    }
-        if (tecla == "d")
-    {
-        document.getElementById("comando").textContent =  ">";
-    }
-        if (tecla == "s")
-    {
-        document.getElementById("comando").textContent =  "V";
-    }
-        if (tecla == "a")
-    {
-        document.getElementById("comando").textContent =  "<";
-    }
+    atas++;
+    document.getElementById("nub").textContent = atas.toString();
 }
